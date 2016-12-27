6eb930d4f0ddf110179a427e51ced17e8ddbaebd
Sean Brennan <zettix@users.noreply.github.com>
2016-12-27 14:21:54 -0800

    Fix doc.

--- src/main/java/com/zettix/graphics/gjkj/hull/Hull.java
+++ src/main/java/com/zettix/graphics/gjkj/hull/Hull.java
@@ -7,15 +7,15 @@ import com.zettix.graphics.gjkj.util.V3;
  * This interface defines a GJK hull with a few benefits:
  * Support(V3): only real part of a GJK hull.
  *
- * ApplyTransform(): Call this to transform hull orientation to internal matrix.
+ * TransformObjectSpace(): Call this to set up the hull with scaling/positioning,rotating to match model.
  *    Suggest you start with new M4().Identity();
- * UpdateTransform(m4): just multiplies the internal transform matrix.
+ * TransformWorldSpace(): Call this when positioning the hull in the world, should be copy of model transform.
  *
- * GetCorner(index) :  actually this sucks, try to remove it.
+ * GetCorner(index) :  For polytopes, returns vertex at index.
  * toString() for debugging. not needed for actual collision detection.
  * toOpenScad() for visualization of results. not needed for actual collision detection.
  *
- * Created by seanbrennan on 11/8/16.
+ * Created by sean on 11/8/16.
  */
 
 public interface Hull {
@@ -23,7 +23,6 @@ public interface Hull {
     V3 GetCorner(int index);
 
     void TransformObjectSpace(M4 m4);
-
     void TransformWorldSpace(M4 m4);
     String toString();
     String toOpenScad(String module_name, boolean hit);
